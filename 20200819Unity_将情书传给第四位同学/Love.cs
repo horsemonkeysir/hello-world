ce55829f487aea56c1e6f038736435e5adc7be34
horsemonkeysir <zhengyang1942@gmail.com>
2020-08-19 14:14:55 +0800

    20080819_3

---
+++ 20200819Unity_将情书传给第四位同学/Love.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Love : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+        for (int i = 1; i <= 4; i++)// “++”的意思：+1
+        {
+            Debug.Log("for情书传给第" + i + "位同学"); 
+        }
+
+        //方法一==================================
+
+        int w = 1;
+        while (w <= 4)
+        {
+            Debug.Log("while情书传给第" + w + "位同学");
+            w++;
+        }
+
+        //方法二====================================
+
+        int d = 1;
+        do
+        {
+            Debug.Log("do while情书传给第" + d + "位同学");
+            d++;
+        }
+        while (d <= 4);
+        //方法三======================================
+
+
+
+
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
