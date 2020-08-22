18ea57e567bafd10f0d690a241151c2d77ed9df9
horsemonkeysir <zhengyang1942@gmail.com>
2020-08-22 11:06:56 +0800

    1

---
+++ 学习内容/20200822Unity_NullReferenceException空物件参考/Text.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Assertions;
+
+public class Text : MonoBehaviour {
+
+	//模拟送礼物的过程
+	void Start () {
+        Person boy = new Person();
+        boy.name = "王大明";
+
+        Person girl = new Person();
+        girl.name = "阿香";
+
+        Present present=new Present();                 
+        present.content = "香水";
+        boy.Give(present,girl);
+	}
+	
+    class Person
+    {
+        public string name;
+        public void Give(Present present, Person to)
+        {
+            Assert.IsNotNull(present.content, "礼物不该是null");//断言 
+            Debug.Log(name + "送了" + present.content + "给" + to.name);
+        }
+    }
+    
+	class Present
+    {
+        public string content;//礼物内容
+    }
+}
