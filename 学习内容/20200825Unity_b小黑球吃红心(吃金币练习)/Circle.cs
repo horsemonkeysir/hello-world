37de234dc7b6c84eaa63cce4d5ad479ca3564dce
horsemonkeysir <zhengyang1942@gmail.com>
2020-08-25 19:48:40 +0800

    a

---
+++ 学习内容/20200825Unity_b小黑球吃红心(吃金币练习)/Circle.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Circle : MonoBehaviour {
+
+    public float moveX;
+    public float moveY;
+    public float push;
+
+    public AudioSource CoinSound;
+
+    Rigidbody2D rb2D;
+
+    void Start() {
+        rb2D = GetComponent<Rigidbody2D>();
+        
+    }
+
+    void FixedUpdate() {
+        moveX = Input.GetAxis("Horizontal");
+        moveY = Input.GetAxis("Vertical");
+        Vector2 movement = new Vector2(moveX, moveY);
+        rb2D.AddForce(push * movement);
+    }
+
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        Debug.Log(name + "触发了" + other.name);
+        if (other.CompareTag("Coin"))
+        {
+            CoinSound.Play();
+            other.gameObject.SetActive(false);
+            
+        }
+    }
+}
