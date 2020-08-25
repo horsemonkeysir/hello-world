d2db9e7e59c39c68c269aa56d4ec99562d728aee
horsemonkeysir <zhengyang1942@gmail.com>
2020-08-25 22:56:56 +0800

    20200825

---
+++ 学习内容/20200825Unity_c移动吃金币练习/BoxCoin.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BoxCoin : MonoBehaviour {
+
+    public float moveX;
+    public float moveY;
+    public float push;
+    Rigidbody2D rb2D;
+
+    public AudioSource Coin;
+
+	void Start () {
+        rb2D = GetComponent<Rigidbody2D>();
+	}
+	
+	
+	void FixedUpdate () {
+        moveX = Input.GetAxis("Horizontal");
+        moveY = Input.GetAxis("Vertical");
+        Vector2 movement = new Vector2(moveX, moveY);
+        rb2D.AddForce(push * movement);
+	}
+
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        Debug.Log(name + "触发了" + other.name);
+
+        if (other.CompareTag("Coin"))
+        {
+            other.gameObject.SetActive(false);
+            Coin.Play();
+        }
+    }
+
+
+}
---
+++ 学习内容/20200825Unity_c移动吃金币练习/CameraController.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraController : MonoBehaviour {
+
+    [Header("玩家物件")]
+    public GameObject player;
+
+    [Header("相对位移")]
+    public Vector3 offset;
+
+	void Start () {
+        offset = transform.position - player.transform.position;
+	}
+	
+	// Update is called once per frame
+	void LateUpdate () {
+        transform.position = player.transform.position + offset;
+	}
+}
