c855aab40f20dfad93508716e5f2805288fdb09c
horsemonkeysir <zhengyang1942@gmail.com>
2020-08-29 23:17:38 +0800

    20200829

--- 学习内容/20200826Unity_小狐狸(入门Tutorial跟做)/Scripts/20200826Unity_小狐狸.cs
+++ 学习内容/20200826Unity_小狐狸(入门Tutorial跟做)/Scripts/20200826Unity_小狐狸.cs
@@ -824,3 +824,294 @@ public class Enemy_Frog : MonoBehaviour
         }
     }
 }
+
+
+16.Class调用(互动包括老鹰制作)
+using System.Collections;
+using System.Collections.Generic;
+using System.Xml.Serialization;
+using UnityEditorInternal;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PlayerController : MonoBehaviour
+{
+    private Rigidbody2D rb;
+    private Animator anim;
+
+    public Collider2D coll;
+    public float speed;
+    public float jumpForce;
+    public LayerMask ground;
+    private int Cherry;
+
+    public Text CherryNum;
+    public bool isHurt;//默认是false
+
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+        anim = GetComponent<Animator>();
+    }
+
+
+    void Update()
+    {
+        if (!isHurt)
+        {
+            Movement();
+        }
+        SwitchAnim();
+    }
+
+    void Movement()
+    {
+        float horizontalMove = Input.GetAxis("Horizontal");
+        float faceDirection = Input.GetAxisRaw("Horizontal");
+
+        //角色移动
+        if (horizontalMove != 0)
+        {
+            rb.velocity = new Vector2(horizontalMove * speed, rb.velocity.y);
+            anim.SetFloat("running", Mathf.Abs(faceDirection));
+        }
+
+        if (faceDirection != 0)
+        {
+            transform.localScale = new Vector3(faceDirection, 1, 1);
+        }
+
+        //角色跳跃
+        if (Input.GetButtonDown("Jump") && coll.IsTouchingLayers(ground))
+        {
+            rb.velocity = new Vector2(rb.velocity.x, jumpForce);
+            anim.SetBool("jumping", true);
+        }
+    }
+
+    //切换动画效果
+    void SwitchAnim()
+    {
+        anim.SetBool("idle", false);
+        if (anim.GetBool("jumping"))
+        {
+            if (rb.velocity.y < 0)
+            {
+                anim.SetBool("jumping", false);
+                anim.SetBool("falling", true);
+            }
+        }
+        else if (isHurt)
+        {
+            anim.SetBool("hurt", true);
+            anim.SetFloat("running", 0);
+            if (Mathf.Abs(rb.velocity.x) < 0.1f)
+            {
+                anim.SetBool("hurt", false);
+                anim.SetBool("idle", true);
+                isHurt = false;
+            }
+        }
+        else if (coll.IsTouchingLayers(ground))
+        {
+            anim.SetBool("falling", false);
+            anim.SetBool("idle", true);
+        }
+    }
+
+    //收集物品
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.tag == "Collection")
+        {
+            Destroy(collision.gameObject);
+            Cherry += 1;
+            CherryNum.text = Cherry.ToString();
+        }
+    }
+
+    //消灭敌人
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        Enemy enemy = collision.gameObject.GetComponent<Enemy>();
+        if (collision.gameObject.tag == "Enemy")
+        {            
+            if (anim.GetBool("falling"))
+            {
+                enemy.JumpOn();
+                rb.velocity = new Vector2(rb.velocity.x, jumpForce);
+                anim.SetBool("jumping", true);
+            }
+            else if (transform.position.x < collision.gameObject.transform.position.x)
+            {
+                rb.velocity = new Vector2(-10, rb.velocity.y);
+                isHurt = true;
+            }
+            else if (transform.position.x > collision.gameObject.transform.position.x)
+            {
+                rb.velocity = new Vector2(10, rb.velocity.y);
+                isHurt = true;
+            }
+        }
+    }
+}
+
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enemy_Frog : Enemy
+{
+    private Rigidbody2D rb;
+    //private Animator Anim;
+    private Collider2D Coll;
+    public LayerMask Ground;
+    public Transform leftpoint, rightpoint;
+    public float Speed,JumpForce;
+    private float leftx, rightx;//方法2
+
+    private bool Faceleft = true;
+
+
+    protected override void Start()
+    {
+        base.Start();
+        rb = GetComponent<Rigidbody2D>();
+        Anim = GetComponent<Animator>();
+        Coll = GetComponent<Collider2D>();
+        //transform.DetachChildren();//方法1,父层和子层取消关联
+        leftx = leftpoint.position.x;//方法2
+        rightx = rightpoint.position.x;
+        Destroy(leftpoint.gameObject);
+        Destroy(rightpoint.gameObject);
+    }
+
+    
+    void Update()
+    {
+        SwitchAnim();
+    }
+
+    void Movement()
+    {
+        if (Faceleft)
+        {
+            if (Coll.IsTouchingLayers(Ground)) 
+            {
+                Anim.SetBool("jumping", true);
+                rb.velocity = new Vector2(-Speed, JumpForce);
+                //if (transform.position.x<leftpoint.position.x)//方法1
+            }
+                if (transform.position.x<leftx)//方法2
+            {
+                transform.localScale = new Vector3(-1, 1, 1);
+                Faceleft = false;
+            }
+        }
+        else
+        {
+            if (Coll.IsTouchingLayers(Ground))
+            {
+                Anim.SetBool("jumping", true);
+                rb.velocity = new Vector2(Speed, JumpForce);                
+            }            
+            if (transform.position.x > rightx)//方法2
+            {
+                transform.localScale = new Vector3(1, 1, 1);
+                Faceleft = true;
+            }
+        }
+    }
+    
+    void SwitchAnim()
+    {
+        if (Anim.GetBool("jumping"))
+        {
+            if (rb.velocity.y<0.1)
+            {
+                Anim.SetBool("jumping", false);
+                Anim.SetBool("falling", true);
+            }
+        }
+        if (Coll.IsTouchingLayers(Ground)&&Anim.GetBool("falling"))
+        {
+            Anim.SetBool("falling", false);
+        }
+    }      
+}
+
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enemy_Eagle : Enemy
+{
+    private Rigidbody2D rb;
+    //private Collider2D coll;
+    public Transform top, bottom;
+    public float Speed;
+    private float TopY, BottomY;
+
+    private bool isUp;
+    
+    protected override void Start()
+    {
+        base.Start();
+        rb = GetComponent<Rigidbody2D>();
+        //coll = GetComponent<Collider2D>();
+        TopY = top.position.y;
+        BottomY = bottom.position.y;
+        Destroy(top.gameObject);
+        Destroy(bottom.gameObject);
+    }
+
+    
+    void Update()
+    {
+        Movement();
+    }
+
+    void Movement()
+    {
+        if (isUp)
+        {
+            rb.velocity = new Vector2(rb.velocity.x, Speed);
+            if (transform.position.y>TopY)
+            {
+                isUp = false;
+            }
+        }
+        else
+        {
+            rb.velocity = new Vector2(rb.velocity.x, -Speed);
+            if (transform.position.y<BottomY)
+            {
+                isUp = true;
+            }
+        }
+    }
+}
+
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enemy : MonoBehaviour
+{
+    protected Animator Anim;
+
+    protected virtual void Start()
+    {
+        Anim = GetComponent<Animator>();
+    }
+
+    public void Death()
+    {
+        Destroy(gameObject);
+    }
+    public void JumpOn()
+    {
+        Anim.SetTrigger("death");
+    }
+}
+
