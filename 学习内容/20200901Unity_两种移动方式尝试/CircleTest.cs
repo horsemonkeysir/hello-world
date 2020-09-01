7587484016ec4d98b7e36afb589e992956d4ffba
horsemonkeysir <zhengyang1942@gmail.com>
2020-09-01 14:02:10 +0800

    20200901

---
+++ 学习内容/20200901Unity_两种移动方式尝试/CircleTest.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Data.SqlTypes;
+using UnityEngine;
+
+public class CircleTest : MonoBehaviour
+{
+    //public float moveX;
+    //public float moveY;
+    //public float push;
+    public float speed;
+
+    Rigidbody2D rb2D;
+    void Start()
+    {
+        rb2D = GetComponent<Rigidbody2D>();
+    }
+
+     void Update()
+    {
+        //moveX = Input.GetAxis("Horizontal");
+        //moveY = Input.GetAxis("Vertical");
+        //Vector2 movement = new Vector2(moveX, moveY);
+        //rb2D.AddForce(push * movement);
+        movement();
+    }
+
+    void movement()
+    {
+        float horizontalMove = Input.GetAxis("Horizontal");
+        float verticalMove = Input.GetAxis("Vertical");
+        if (horizontalMove != 0 || verticalMove !=0)
+        {
+            rb2D.velocity = new Vector2(horizontalMove * speed, verticalMove*speed);            
+        }
+        
+    }
+}
