1f5d8b2dc3f2930f6dcc4640027fd9f9a2e299fe
horsemonkeysir <zhengyang1942@gmail.com>
2020-09-04 11:23:35 +0800

    a

---
+++ 学习内容/20200903Unity_移动加动画练习/ColaTest.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ColaTest : MonoBehaviour
+{
+    public float moveX;
+    public float moveY;
+    //public float push;
+    public float speed;
+
+    Rigidbody2D rb2D;
+    void Start()
+    {
+        rb2D = GetComponent<Rigidbody2D>();
+    }
+
+    void Update()
+    {
+        moveX=Input.GetAxis("Horizontal");
+        moveY = Input.GetAxis("Vertical");
+        //Vector2 movement = new Vector2(moveX, moveY);
+        //rb2D.AddForce(push * movement);
+        if (moveX !=0 || moveY!=0)
+        {
+            rb2D.velocity = new Vector2(moveX * speed, moveY * speed);
+        }
+    }
+}
